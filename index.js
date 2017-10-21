9d7d86f02350cfdb2f8059d04e0174da870d7354
Haley West <haleykat09@yahoo.com>
2017-10-21 00:19:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function holidayCountdown() {
 var name = "you"
 
 var holiday = {
-  if (string === `It's ${days} until ${holiday}!`) {return `Independence Day`}
-  else if (string === `Happy ${holiday}, ${name}!`) {return `Mother\'s Day`}
+  if (string === "It's ${days} until ${holiday}!") {return `Independence Day`}
+  else if (string === "Happy ${holiday}, ${name}!") {return `Mother\'s Day`}
 }
